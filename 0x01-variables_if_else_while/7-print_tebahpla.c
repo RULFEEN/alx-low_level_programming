838c98ba74fba513f322836a66d7341aa0e2defa
Victor Mayi <victormay.i.797@gmail.com>
2023-02-17 09:32:57 -0800

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - prints the alphabet in reverse
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+char c;
+
+c = 'z';
+
+while (c >= 'z')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+
+return (0);
+}
