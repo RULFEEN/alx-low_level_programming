3ea5e75ef1cf1326c228c7b00039ab4e1b34457f
Victor Mayi <victormay.i.797@gmail.com>
2023-02-21 09:39:57 -0800

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Function that prints alphabet times 10
+ *
+ *Return: no return value
+ */
+
+void print_alphabet_x10(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar('c');
+c++;
+}
+_putchar('\n');
+}
+
+}
