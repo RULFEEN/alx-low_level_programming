cad00ffd26a976f1541813de3f72359523314ed9
Victor Mayi <victormay.i.797@gmail.com>
2023-02-21 12:59:44 -0800

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,44 +1,46 @@
 #include "main.h"
+
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: the number to start from
+ *print_to_98 - Function that prints integers to 98
+ *
+ *@n: number to start from
  */
+
 void print_to_98(int n)
 {
 int i;
-if (n == 98)
-{
-_putchar(n / 10 + '0');
-_putchar(n % 10 + '0');
-_putchar('\n'); }
 if (n > 98)
 {
 for (i = n; i >= 98; i--)
 {
 if (i < 10)
 _putchar(i + '0');
+else if (i > 100)
+{ _putchar(i / 100 + '0');
+_putchar((i % 100) / 10 + '0');
+_putchar(i % 10 + '0'); }
 else
-{
-_putchar(i / 10 + '0');
+{ _putchar(i / 10 + '0');
 _putchar(i % 10 + '0'); }
 if (i != 98)
-{
-_putchar(',');
+{ _putchar(',');
 _putchar(' '); }}
 _putchar('\n'); }
-else
+else if (n < 98)
 {
 for (i = n; i <= 98; i++)
 {
 if (i < 10)
 _putchar(i + '0');
 else
-{
-_putchar(i / 10 + '0');
+{ _putchar(i / 10 + '0');
 _putchar(i % 10 + '0'); }
 if (i != 98)
-{
-_putchar(',');
-_putchar(' '); }}}
-_putchar('\n');
+{ _putchar(',');
+_putchar(' ');
+_putchar('\n'); }}}
+else
+{ _putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
+_putchar('\n'); }
 }
