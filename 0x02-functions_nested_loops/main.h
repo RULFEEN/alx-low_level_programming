f3ab4b4f366e538357fcbc01c56754c66efcba66
Victor Mayi <victormay.i.797@gmail.com>
2023-02-21 09:18:54 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
+
+/* Function prototypes */
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+
+#endif
+
