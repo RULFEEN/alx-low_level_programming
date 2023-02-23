0594be98994af9c539c3cdfb7baf6a72238f409c
Victor Mayi <victormay.i.797@gmail.com>
2023-02-22 23:54:42 -0800

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,6 @@
 void print_remaining_days(int month, int day, int year)
 {
 int days_in_feb = 28;
-
 int days_in_year = 365;
 
 if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
@@ -22,25 +21,11 @@ days_in_feb = 29;
 days_in_year = 366;
 }
 
-if (month < 1 || month > 12)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-return;
-}
-
-if (day < 1 || day > 31 ||
-(month == 4 || month == 6 || month == 9 || month == 11) && day == 31 ||
-(month == 2 && day > days_in_feb))
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-return;
-}
-
 day = convert_day(month, day);
 
 if (days_in_feb == 29 && month > 2)
 {
-day++;
+day += 1;
 }
 
 printf("Day of the year: %d\n", day);
