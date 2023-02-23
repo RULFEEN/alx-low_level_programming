c70802b733c02f05517a0eb6b18a521785965a55
Victor Mayi <victormay.i.797@gmail.com>
2023-02-22 23:41:33 -0800

    print_remaining_days

---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ *print_remaining_days - prints the remianing days in a year
+ *
+ *@month: the month of date
+ *@day: the day of current date
+ *@year: the year
+ *
+ *Return: void return
+ */
+
+void print_remaining_days(int month, int day, int year)
+{
+int days_in_feb = 28;
+
+int days_in_year = 365;
+
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+{
+days_in_feb = 29;
+days_in_year = 366;
+}
+
+if (month < 1 || month > 12)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+return;
+}
+
+if (day < 1 || day > 31 ||
+(month == 4 || month == 6 || month == 9 || month == 11) && day == 31 ||
+(month == 2 && day > days_in_feb))
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+return;
+}
+
+day = convert_day(month, day);
+
+if (days_in_feb == 29 && month > 2)
+{
+day++;
+}
+
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", days_in_year - day);
+return;
+}
