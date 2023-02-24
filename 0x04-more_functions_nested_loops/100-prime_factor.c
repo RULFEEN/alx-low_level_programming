c244615ab6a3228f00c8be58b95c0fc062dca554
Victor Mayi <victormay.i.797@gmail.com>
2023-02-24 09:15:32 -0800

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+long int num = 612852475143;
+int factor = 2;
+int largest_factor = 0;
+
+while (num != 1)
+{
+if (num % factor == 0)
+{
+largest_factor = factor;
+num = num / factor;
+}
+else
+{
+factor++;
+}
+}
+
+printf("%d\n", largest_factor);
+
+return (0);
+}
