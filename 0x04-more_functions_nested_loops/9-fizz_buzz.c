20d73433abf7d1a895e461ef15adf3f5dd46b6f3
Victor Mayi <victormay.i.797@gmail.com>
2023-02-24 09:01:43 -0800

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,12 +16,17 @@ printf("FizzBuzz ");
 else if (i % 3 == 0)
 printf("Fizz ");
 else if (i % 5 == 0)
+{
+if (i == 100)
+printf("Buzz");
+else
 printf("Buzz ");
+}
 else
 printf("%d ", i);
 }
 
-putchar('\n');
+printf("\n");
 
 return (0);
 }
