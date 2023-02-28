098cb214ce50aa8f07d42b416b6dad549a4150c8
Victor Mayi <victormay.i.797@gmail.com>
2023-02-28 00:59:10 -0800

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,13 +10,18 @@
 
 char *_strcpy(char *dest, char *src)
 {
-int len = 0, i, j;
+  int len = 0, dlen = 0, i, j, k;
 
 for (i = 0; src[i] != '\0'; i++)
 len++;
 
 for (j = 0; j < len; j++)
 dest[j] = src[j];
+
+for (k = 0; dest[k] != '\0'; k++)
+dlen++;
+for ( ; j < dlen; j++)
+dest[j] = '\0';
 
 return (dest);
 }
