7976fa34272db36bbf50fcf865b56bf43b2a06b9
Victor Mayi <victormay.i.797@gmail.com>
2023-02-27 23:42:04 -0800

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+int len = 0;
+int half;
+
+for (i = 0; str[i] != '\0'; i++)
+len++;
+
+if (len % 2 == 0)
+half = len / 2;
+else
+half = (len / 2) + 1;
+
+for (j = half; str[j] != '\0'; j++)
+_putchar(str[j]);
+
+_putchar('\n');
+}
