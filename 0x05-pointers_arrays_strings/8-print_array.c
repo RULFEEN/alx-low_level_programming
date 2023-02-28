2b8f4ac38ea80b8827756eff93e86412370a31f5
Victor Mayi <victormay.i.797@gmail.com>
2023-02-27 23:58:01 -0800

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_array - prints an array contiguously
+*
+*@a: an array of integers
+*@n: number of elements of array to be printed
+*Return: void
+*/
+
+void print_array(int *a, int n)
+{
+int i;
+  
+for(i = 0; i < n; i++)
+printf(a[i]);
+}
