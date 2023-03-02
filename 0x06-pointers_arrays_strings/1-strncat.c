ff383af22ec7a43a1eb0e865b2720959a256b3ce
Victor Mayi <victormay.i.797@gmail.com>
2023-03-01 22:46:39 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strncat - Function that appends second string up to n
+ *@dest: Mutable string
+ *@src: String mutation source
+ *
+ *Return: mutated string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int dlen = 0, i, j;
+
+for (i = 0; dest[i] != '\0'; i++)
+dlen++;
+
+for (j = 0; j < n && src[j] != '\0'; j++)
+dest[dlen + j] = src[j];
+dest[dlen + j] = '\0';
+
+return (dest);
+}
