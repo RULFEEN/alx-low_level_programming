e7aaac9485d0b2035dfc390523fcf2f8eb6292fa
Victor Mayi <victormay.i.797@gmail.com>
2023-03-02 02:18:13 -0800

    string toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *string_toupper - function that converts lowercase to uppercase
+ *@str: The string containing lowercase letters
+ *
+ *Return: The string
+ */
+
+char *string_toupper(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0') {
+if (str[i] >= 'a' && str[i] <= 'z') {
+str[i] = str[i] - ('a' - 'A');
+}
+i++;
+}
+
+return (str);
+}
