ea3d68e118b0c1a4180e013995721ad177c3d7f6
Victor Mayi <victormay.i.797@gmail.com>
2023-03-14 05:21:01 -0700

    No of arhs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - function that returnes number of arguments
+ *@argc: number of arguments plus name of program
+ *@argv: array of all arguments
+ *
+ *Return: Success (0)
+ */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+  printf("%d\n", (argc - 1));
+  return (0);
+}
