fbf57fd1bd655bb97fa9016f76ea570c65ee20e2
Victor Mayi <victormay.i.797@gmail.com>
2023-03-14 05:45:30 -0700

    Mult args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - multiplies arguments
+ *@argc: number of all arguments
+ *@argv: string of arguments
+ *
+ *Return: Success (0), or Error (1)
+ */
+
+int main (int argc, char *argv[])
+{
+int prod;
+if (argc == 3)
+{
+sum = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", prod);
+return (0);
+}
+
+else
+{
+printf("Error\n");
+return (1);
+}
+}
