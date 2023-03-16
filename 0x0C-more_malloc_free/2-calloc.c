63f0bf1a3d2e5d1f4e578b9b302bf6e9c8648cf1
Victor Mayi <victormay.i.797@gmail.com>
2023-03-16 11:55:48 -0700

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *_calloc - malloc's calloc pseudo-code
+ *@nmemb: number of elements
+ *@size: size of one element in bytes
+ *
+ *Return: pointer to address
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *foo;
+unsigned int i, tsize;
+
+tsize = nmemb * size;
+if (tsize == 0)
+return (NULL);
+
+foo = malloc(tsize + 1);
+if (foo == NULL)
+return (NULL);
+
+for (i = 0; i < (tsize + 1); i++)
+foo[i] = 0;
+foo[i] = '\0';
+
+return (foo);
+}
