f329b673260af4cfe6b170c1c3e8a47dcc6070b1
Victor Mayi <victormay.i.797@gmail.com>
2023-03-20 18:10:28 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog - a declaration for a structure "dog"
+ *@name: dog's name
+ *@age: dog's age as a float
+ *@owner: owner of dog
+ *
+ *Description: Initializes dog with name, age and owner
+ */
+
 struct dog
 {
-  char *name;
-  float age;
-  char *owner
+char *name;
+float age;
+char *owner;
 };
 
 #endif
