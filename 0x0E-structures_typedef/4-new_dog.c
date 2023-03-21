b5215249e37ecc5f963ee7f3a506d6a78b52553c
Victor Mayi <victormay.i.797@gmail.com>
2023-03-21 07:51:56 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,9 +18,22 @@ dog2 = malloc(sizeof(dog_t));
 if (dog2 == NULL)
 return (NULL);
 
-dog2->name = name;
-dog2->age = age;
-dog2->owner = owner;
+new_dog->name = strdup(name);
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+
+new_dog->owner = strdup(owner);
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
+
+new_dog->age = age;
 
 return (dog2);
 }
